5045e504c41b15b252e15f7de06d6245024d1a1f
Sergio Issi <sergio.issi@gmail.com>
2020-08-07 14:55:08 +0200

    :octocat: test

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name='testlib',
-    version='0.0.1',
+    version='0.0.2',
 
     descritption="A test CDK Python app",
     long_description=long_description,

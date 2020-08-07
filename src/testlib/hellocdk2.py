08582d807b592ee2b54459772e3b76d651652c23
Sergio Issi <sergio.issi@gmail.com>
2020-08-07 15:35:19 +0200

    :octocat: test

--- src/testlib/hellocdk2.py
+++ src/testlib/hellocdk2.py
@@ -9,4 +9,8 @@ class TestlibcdkStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        bucket = s3.Bucket(self, "testlibcdkbucketzord")
+        bucket = s3.Bucket(
+            self,
+            "testlibcdkbucketzord",
+            bucket_name="bucketname"
+        )

a33bf0d9331e3f567133c095d130fe8fa6a9ff29
Sergio Issi <sergio.issi@gmail.com>
2020-08-12 17:10:15 +0200

    :octocat: test

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    package_dir={'': 'src'},
+    # package_dir={'': 'src'},
     packages=find_packages(),
 
     install_requires=[
@@ -51,7 +51,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    package_dir={'': 'src'},
+    # package_dir={'': 'src'},
     packages=find_packages(),
 
     install_requires=[

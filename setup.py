3e3f71eecbff13e30e972e4b207773e416c4a3f8
Sergio Issi <sergio.issi@gmail.com>
2020-08-13 11:10:45 +0200

    :octocat: test

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fp:
 
 setup(
     name='devopscdk',
-    version='0.0.2',
+    version='0.0.3',
 
     long_description=long_description,
     long_description_content_type="text/markdown",
 
-    package_dir={'devopscdk': 'src'},
+    package_dir={'': 'src'},
     packages=find_packages(where="src"),
 
     install_requires=[

ffe75d73fcf5eb0bcf9bf3ccb9ab625db6dea8a2
Sergio Issi <sergio.issi@gmail.com>
2020-08-13 11:08:15 +0200

    :octocat: test

--- setup.py
+++ setup.py
@@ -5,26 +5,6 @@ with open("README.md", "r") as fp:
     long_description = fp.read()
 
 setup(
-    name='testlib',
-    version='0.0.12',
-
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-
-    package_dir={'testlib': 'src'},
-    packages=find_packages(where="src/testlib"),
-
-    install_requires=[
-        "aws-cdk.core",
-        "aws-cdk.aws-s3",
-        "aws-cdk.aws-sns"
-    ],
-
-    python_requires=">=3.6",
-
-)
-
-setup(
     name='devopscdk',
     version='0.0.2',
 
@@ -32,7 +12,7 @@ setup(
     long_description_content_type="text/markdown",
 
     package_dir={'devopscdk': 'src'},
-    packages=find_packages(where="src/devopscdk"),
+    packages=find_packages(where="src"),
 
     install_requires=[
         "aws-cdk.core",
